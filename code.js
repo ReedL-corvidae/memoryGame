2804c4cc067289cb05114379b0abc190e2298a51
ReedL-corvidae <rclinehan@email.neit.edu>
2025-11-13 09:46:29 -0500

    second commit

---
+++ code.js
@@ -0,0 +1,10 @@
+import {Card} from "./card.js";
+
+let canvas = document.getElementById("myCanvas");
+let pencil = canvas.getContext("2d");
+let card = new Card();
+
+function gameLoop(){
+
+}
+setInterval(gameLoop, 50);
