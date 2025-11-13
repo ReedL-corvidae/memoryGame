c8dc3b91f268f7585049e685529e8d66e76528a1
ReedL-corvidae <rclinehan@email.neit.edu>
2025-11-13 10:26:27 -0500

    card stuff

--- card.js
+++ card.js
@@ -1,6 +1,27 @@
 export class Card {
 
-        getRandomCardColor(){
+    x = 10;
+    y = 10;
+    width = 100;
+    height = 150;
+    canvas;
+    pencil;
+
+     constructor(canvas, pencil) {
+        this.canvas = canvas;
+        this.pencil = pencil;
+    }
+
+    drawCard(){
+        this.pencil.fillStyle = "black";
+        this.pencil.fillRect(
+            this.x, 
+            this.y, 
+            this.width, 
+            this.height
+        ); // x, y, w, h
+    }
+    getRandomCardColor(){
         let red = Math.floor(Math.random() * 256);
         let green = Math.floor(Math.random() * 256);
         let blue = Math.floor(Math.random() * 256);
--- code.js
+++ code.js
@@ -2,9 +2,17 @@ import {Card} from "./card.js";
 
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
-let card = new Card();
+let card = new Card(canvas, pencil);
+let cards = [];
+
 
 function gameLoop(){
+
+    pencil.clearRect(0, 0, canvas.width, canvas.height);
+
+    card.drawCard();
+
+
 
 }
 setInterval(gameLoop, 50);
